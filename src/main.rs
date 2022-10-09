706e56b9cf34d469c44232fb0f37a18851597ec9
nozwock <57829219+nozwock@users.noreply.github.com>
2022-10-09 17:50:22 +0530

    feat: use eframe_template to get started

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use eframe::egui;
+
+fn main() {
+    // let native_options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        // Hide the OS-specific "chrome" around the window:
+        // decorated: true,
+        // To have rounded corners we need transparency:
+        // transparent: true,
+        min_window_size: Some(egui::vec2(350.0, 100.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Ventoy Toybox App",
+        options,
+        Box::new(|cc| Box::new(ventoy_toybox::ToyboxApp::new(cc))),
+    );
+}
