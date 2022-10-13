ead27db14d4944baac2a17bfda88ef975b07f990
nozwock <57829219+nozwock@users.noreply.github.com>
2022-10-13 20:43:10 +0530

    feat: use release feeds from github

--- src/gui/app.rs
+++ src/gui/app.rs
@@ -1,29 +1,28 @@
 use crate::core::utils;
 use eframe::egui::{self, RichText, ScrollArea};
+use poll_promise::Promise;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 // #[derive(serde::Deserialize, serde::Serialize)]
 // #[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[derive(Default)]
 pub struct ToyboxApp {
     curr_frame: AppFrame,
     config: ToyboxAppConfig,
     filter_release: String,
     release_groups: Vec<String>,
     release_curr_group_idx: usize,
-    // Example stuff:
-    // label: String,
-
-    // this how you opt-out of serialization of a member
-    // #[serde(skip)]
-    // value: f32,
+    // release_promise: Option<Promise<ehttp::Result<Vec<FeedsItem>>>>, 
 }
 
+#[derive(Default)]
 pub struct ToyboxAppConfig {
     pub release_feeds: Vec<utils::FeedsItem>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Default)]
 enum AppFrame {
+    #[default]
     VentoyUpdate,
     ReleaseBrowse,
 }
@@ -39,44 +38,53 @@ impl ToyboxApp {
         // if let Some(storage) = cc.storage {
         //     return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         // }
-
         // Self::default()
 
-        let dummy_feeds = (1..=250).map(|x| utils::FeedsItem {
-            group: format!("?group{}", x),
-            name: format!("some-xyz-linux-distro-etc{}.iso.torrent", x),
-            torrent_url: format!(
-                "https://blahblahblah.uwu/some-xyz-linux-distro-etc{}.iso.torrent",
-                x
-            ),
-            magnet: format!("magnet:?xt=2fhSomeRandomChars8ru1ur10rh01g0930g093weg{}", x),
-            date: format!("?date{}", x),
-        });
-        let mut dummy_groups = dummy_feeds
-            .clone()
-            .map(|x| x.group)
-            .collect::<Vec<String>>();
-        dummy_groups.insert(0, "all".to_owned());
-        let dummy_feeds = Vec::from_iter(dummy_feeds);
-
-        // let feeds = utils::Feeds::new().unwrap();
-        // let mut groups = feeds
+        // let dummy_feeds = (1..=250).map(|x| utils::FeedsItem {
+        //     group: format!("?group{}", x),
+        //     name: format!("some-xyz-linux-distro-etc{}.iso.torrent", x),
+        //     torrent_url: format!(
+        //         "https://blahblahblah.uwu/some-xyz-linux-distro-etc{}.iso.torrent",
+        //         x
+        //     ),
+        //     magnet: format!("magnet:?xt=2fhSomeRandomChars8ru1ur10rh01g0930g093weg{}", x),
+        //     date: format!("?date{}", x),
+        // });
+        // let mut dummy_groups = dummy_feeds
         //     .clone()
-        //     .into_iter()
         //     .map(|x| x.group)
         //     .collect::<Vec<String>>();
-        // groups.insert(0, "all".to_owned());
+        // dummy_groups.insert(0, "all".to_owned());
+        // let dummy_feeds = Vec::from_iter(dummy_feeds);
 
-        // use poll promise for waiting while u fetch those juicy json
+        // poor main thread T~T
+        let feeds = utils::Feeds::new().unwrap();
+        let mut group_duplicates: Vec<String> = Vec::new();
+        let mut groups: Vec<String> = feeds
+            .clone()
+            .into_iter()
+            .map(|x| x.group)
+            .filter(|x| {
+                for t in &group_duplicates {
+                    if t == x {
+                        return false;
+                    }
+                }
+                group_duplicates.push(x.clone());
+                true
+            })
+            .collect();
+        groups.insert(0, "all".to_owned());
 
         Self {
-            release_groups: dummy_groups,
+            release_groups: groups,
             release_curr_group_idx: 0,
             filter_release: String::new(),
             curr_frame: AppFrame::ReleaseBrowse,
             config: ToyboxAppConfig {
-                release_feeds: dummy_feeds,
+                release_feeds: feeds,
             },
+            ..Default::default()
         }
     }
 
@@ -116,6 +124,9 @@ impl ToyboxApp {
                 AppFrame::ReleaseBrowse,
                 "🔍  Browse OS Releases",
             );
+            ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
+                egui::warn_if_debug_build(ui);
+            });
         });
         ui.separator();
     }
@@ -158,18 +169,35 @@ impl eframe::App for ToyboxApp {
         //     filter_release: filter_entry,
         //     curr_frame,
         //     config,
-        //     label,
-        //     value,
+        //     release_groups,
+        //     release_curr_group_idx,
+        //     release_promise,
         // } = self;
 
         self.configure_fonts(ctx);
 
+        // i hate the way this is being done... T~T
+        // let release_promise = self.release_promise.get_or_insert_with(|| { // mutable borrow here aswell
+        //     let ctx = ctx.clone();
+        //     let (sender, promise) = Promise::new();
+        //     // let response = utils::Feeds::new();
+        //     let request = ehttp::Request::get("https://github.com/nozwock/ventoy-toybox-feed/releases/download/feeds/releases.json");
+        //     ehttp::fetch(request, move |response| {
+        //         let release_feeds  = response.and_then(|response| {
+        //             let r: Vec<FeedsItem> = serde_json::from_str(response.text().unwrap()).unwrap();
+        //             Ok(r)
+        //         });
+        //         sender.send(release_feeds);
+        //         ctx.request_repaint();
+        //     }) ;
+        //     promise
+        // });
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.render_header(ui);
+            self.render_header(ui); // mutable borrow here
             match self.curr_frame {
                 AppFrame::VentoyUpdate => {}
                 AppFrame::ReleaseBrowse => {
-                    egui::warn_if_debug_build(ui);
                     ui.horizontal(|ui| {
                         ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
                             ui.collapsing("Filter", |ui| {
@@ -202,9 +230,7 @@ impl eframe::App for ToyboxApp {
 
                     ScrollArea::vertical()
                         .auto_shrink([false; 2])
-                        .show(ui, |ui| {
-                            self.render_release_cards(ui);
-                        });
+                        .show(ui, |ui| self.render_release_cards(ui));
                 }
             }
         });
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ use eframe::egui;
 
 fn main() {
     // let native_options = eframe::NativeOptions::default();
-    let native_options = eframe::NativeOptions {
+    let options = eframe::NativeOptions {
         // Hide the OS-specific "chrome" around the window:
         // decorated: false,
         // To have rounded corners we need transparency:
         // transparent: true,
-        min_window_size: Some(egui::vec2(500.0, 250.0)),
         initial_window_size: Some(egui::vec2(500.0, 250.0)),
+        min_window_size: Some(egui::vec2(500.0, 250.0)),
         ..Default::default()
     };
     eframe::run_native(
         "Ventoy Toybox",
-        native_options,
+        options,
         Box::new(|cc| Box::new(app::ToyboxApp::new(cc))),
     );
 }
