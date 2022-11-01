b05faf91ffc20b31c746fa272080477b9deb77d5
nozwock <57829219+nozwock@users.noreply.github.com>
2022-11-01 13:46:51 +0530

    chore: update build script

--- build.rs
+++ build.rs
@@ -4,14 +4,19 @@ use winres::WindowsResource;
 // https://github.com/mxre/winres/issues/33
 
 fn main() -> io::Result<()> {
-    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows"
-        && std::env::var("CARGO_CFG_TARGET_ENV").unwrap() == "gnu"
-    {
-        WindowsResource::new()
-            .set_ar_path("x86_64-w64-mingw32-ar")
-            .set_windres_path("x86_64-w64-mingw32-windres")
-            .set_icon("assets/icon.ico")
-            .compile()?;
+    if std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap() == "windows" {
+        let mut res = WindowsResource::new();
+        match std::env::var("CARGO_CFG_TARGET_ENV").unwrap().as_str() {
+            "gnu" => {
+                #[cfg(not(target_os = "windows"))]
+                res.set_ar_path("x86_64-w64-mingw32-ar")
+                    .set_windres_path("x86_64-w64-mingw32-windres");
+            }
+            "msvc" => {}
+            _ => panic!("unsupported env"),
+        };
+        res.set_icon("assets/icon.ico");
+        res.compile()?;
     }
     Ok(())
 }
